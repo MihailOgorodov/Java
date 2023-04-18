5345b963d4be937520f49cdc36540589c38d6f0c
“Mihail <mic311089@gmail.com>
2023-04-18 11:07:06 +0400

    lec2 added

--- src/main/java/org/example/sem2/first.java
+++ src/main/java/org/example/sem2/first.java
@@ -1,7 +1,13 @@
 package org.example.sem2;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class first {
-    public static void main(String[] args) {
-        System.out.println("hi");
-    }
+//    public static void main(String[] args) throws IOException { // чтобы не было ошибки при обращении к файлу, если он битый, занят или что-то еще
+//        FileReader f = new FileReader(g); // читать файл
+//        FileWriter w = new FileWriter(g); // писать файл
+
+
 }
